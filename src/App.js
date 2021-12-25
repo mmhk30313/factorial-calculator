939c47318751bbb4e662d2c957c17a8bdf9f30d6
mmhk30313 <mmhk.mmhkitsforyou@gmail.com>
2021-12-25 17:25:21 +0600

    ReadMe.md file should be updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
+export default function App() {
+  const [factResult, setFactResult] = useState("   ");
+  const factorial = (form) => {
+    form.preventDefault();
+    // const elements = form?.target?.elements;
+    try{
+      const n = form?.target?.int_number?.value;
+      console.log({n});
+      if(Number(n) < 0 || isNaN(n) || parseFloat(n) % 1 || !isFinite(n)){
+        setFactResult(n + " is not a positive integer");
+        return;
+      }
+      if(parseInt(n) > 120000){
+        setFactResult("Input number should not be greater then 120000 for working smoothly");
+        return;
+      }
+      let p = BigInt(1)
+      let i = BigInt(parseInt(n))
+    // console.log({p});
+    // console.log({n});
+      while (1 < i){ p *= i; i--;}
+      // console.log(n+"! = ",p);
+      setFactResult(n + "! = " + String(p))
+      return;
+    }catch(err){
+      setFactResult("Enter a valid positive integer!!!")
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: '0', padding: '5px'}}>
+      <h1>Factorial Calculator</h1>
+      <form onSubmit={(form) => {factorial(form)}}>
+        <input type="number" name="int_number" placeholder="Enter a number..." />
+        <br /><br/>
+        <button type="submit">Calculate Factorial</button>
+      </form>
+      <h2 style={{width: "100%", padding: '2px', overflowX: 'scroll'}}>Factorial: {factResult}</h2>    
     </div>
   );
 }
-
-export default App;
